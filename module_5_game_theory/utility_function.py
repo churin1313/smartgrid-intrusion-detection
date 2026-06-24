159fe15f515e6d94ce2089e58f606c62ce454492
churin1313 <just.a.piece.of.bacon265@gmail.com>
2026-06-24 18:06:21 +0530

    Create utility_function.py

---
+++ module_5_game_theory/utility_function.py
@@ -0,0 +1,11 @@
+payoff_matrix = {
+    ("Attack", "Monitor"): (-2, 2),
+    ("Attack", "Isolate Node"): (-5, 5),
+    ("Attack", "Reconfigure Grid"): (-4, 4),
+    ("Attack", "Trigger Alert"): (-3, 3),
+
+    ("Stay Silent", "Monitor"): (0, -1),
+    ("Stay Silent", "Isolate Node"): (0, -2),
+    ("Stay Silent", "Reconfigure Grid"): (0, -2),
+    ("Stay Silent", "Trigger Alert"): (0, -1)
+}
